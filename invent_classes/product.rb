911a47b50c26c1492835397566a03fcd5a4f3460
Ethan <ethanjneyland@gmail.com>
2022-04-14 14:19:34 +1000

    product and stocklist class added

---
+++ invent_classes/product.rb
@@ -0,0 +1,24 @@
+
+
+class Product
+
+    attr_reader :id, :make, :model, :type
+    attr_accessor :quantity
+
+    def initialize(id, make, model, type, quantity)
+        @id = id
+        @make = make
+        @model = model
+        @type = type
+        @quantity = quantity
+    end
+
+    def display_product
+        return "#{@id}" +" "*(12 - @id.length)+  "#{@make}" +" "*(10 - @make.length)+ "#{@model}" +" "*(20 - @model.length)+ "#{@type}" +" "*(20 - @type.length)+ "#{@quantity}"
+    end
+
+    def to_s
+        return "#{@id}"
+    end
+
+end
--- invent_classes/stocklist.rb
+++ invent_classes/stocklist.rb
@@ -1,1 +1,43 @@
 
+
+class Stocklist
+    attr_reader :name, :products
+
+    def initialize(name, products)
+        @name = name
+        @products = products
+    end
+
+    def add_product(id, make, model, type, quantity)
+        product = Product.new(id, make, model, type, quantity)
+        @products << product
+    end
+
+    # invoices = JSON.load_file("./sample_invoices.json", symbolize_keys: true)
+
+    def update_product_quantity(id, quantity)
+        p quantity
+        @products = @products.map do |product|
+            p product
+            if product.id == id
+                product.quantity = quantity
+            else
+                product
+            end
+        end
+    end
+
+    def get_quantity_by_id(id)
+        item = @products.find {|product| product.id == id}
+        return item.quantity
+    end
+
+    def display_stocklist
+        break_line
+        puts " "*23 + "| " + Rainbow(@name).green + " |"
+        break_line
+        puts Rainbow("ID").orange + " "*10 + "Make" + " "*6 + "Model" + " "*15 + "Type" + " "*15 + Rainbow("Qty").red
+        break_line
+    end
+
+end
